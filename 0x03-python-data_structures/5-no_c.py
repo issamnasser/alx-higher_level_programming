691901131d8a6ee59b4a12ca24a2af27501f25d1
issamnasser <133253756+issamnasser@users.noreply.github.com>
2023-12-02 22:01:54 +0100

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        noc = my_string.translate({ord(i): None for i in 'cC'})
-        return noc
+    noc = my_string.translate({ord(i): None for i in 'cC'})
+    return noc
