c9079b8546f7be1e5f740c4e7bdad5600f1e0645
issamnasser <issamnasser56@gmail.com>
2023-12-03 10:36:38 -0800

    nwcommit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
+#!/usr/bin/python3
 def multiple_returns(sentence):
-    tup = ()
-    if len(sentence) == 0:
-        tup = 0, "None"
+    returns=()
+    if not sentence:
+        return returns(len(sentence),None)
     else:
-        tup = len(sentence), sentence[0]
-    return tup
+        return returns(len(sentence),sentence[0])
