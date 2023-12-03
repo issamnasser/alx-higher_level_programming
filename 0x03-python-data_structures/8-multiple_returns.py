7228f17f01746dab2985dffaf839f18f03e77f4f
issamnasser <issamnasser56@gmail.com>
2023-12-03 10:37:50 -0800

    nnwcommit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     returns=()
     if not sentence:
-        return returns(len(sentence),None)
+        return returns(len(sentence),"None")
     else:
         return returns(len(sentence),sentence[0])
