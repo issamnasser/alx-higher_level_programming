1f0b2f8a4410505fdbf2b43319ce6cf6b278a972
issamnasser <133253756+issamnasser@users.noreply.github.com>
2023-12-03 20:39:36 +0100

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    t_f = my_list
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            t_f[i] = True
+        else:
+            t_f[i] = False
+    return t_f
